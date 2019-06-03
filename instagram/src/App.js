703616e0225b3558f0854c18886ae1365ddaa2b8
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-03 20:44:09 +0200

    added icons and styling for all components

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './App.css';
 import dummyData from './dummy-data';
 import PropTypes from 'prop-types';
-
+import logo from './instagram.png';
 class App extends React.Component {
   constructor() {
     super();
@@ -13,7 +13,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className="App">
+      <div className="app">
         <SearchBar />
         {this.state.instaData.map(data =>
           <PostContainer dummyData={data} />
@@ -27,12 +27,24 @@ export default App;
 
 const PostContainer = props => {
   return (
-    <div key={props.dummyData.id}>
-      <p>{props.dummyData.username}</p>
-      <img src={props.dummyData.thumbnailUrl} alt="Header" />
-      <img src={props.dummyData.imageUrl} alt="Post" />
-      <p>{props.dummyData.likes}</p>
+    <div>
+      <div className="post-top">
+      <img src={props.dummyData.thumbnailUrl} alt="Thumbnail" className="thumbnail" />
+      <p className="username">{props.dummyData.username}</p>
+      </div>
+      <img src={props.dummyData.imageUrl} alt="Post" className="post-img" />
+      <div className="icons">
+        <div className="left-icons">      
+          <p className="entypo-heart-empty"></p>
+      <p className="entypo-comment "></p>
+      </div>
+      <div className="flex-end">
+        <p className="entypo-bookmark"></p>
+        </div>
+      </div>
+      <p>{props.dummyData.likes} likes</p>
       <CommentSection dummyData={props.dummyData.comments} />
+      <input className="post-input"></input>
     </div>
   )
 }
@@ -64,10 +76,18 @@ CommentSection.propTypes = {
 const SearchBar = () => {
   return (
     <div className="search-bar">
-      <p>Instagram</p>
+           <div className="left-header"> 
+        <p className="entypo-instagrem"></p>
+      <img className="insta-picture" src={logo} alt="insta-logo" />
+      </div>
+      
       <input placeholder="Search"></input>
+     
+      <div className="right-header">
+      <p className="entypo-heart-empty header-icon"></p>
+      <p className="entypo-user header-icon"></p>
+      <p className="entypo-compass header-icon"></p>
+      </div>
     </div>
   )
-
-
 }
