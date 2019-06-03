2594c7e9ededaf1c5504c483d350f39ea6f5d015
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-03 14:03:18 +0200

    start of insta clone

---
+++ instagram/src/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <SearchBar></SearchBar>
+      <PostContainer></PostContainer>
+      <CommentSection></CommentSection>
+    </div>
+  );
+}
+
+export default App;
+
+function SearchBar() {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+function PostContainer() {
+  return(
+    <div>
+      
+    </div>
+  )
+}
+
+function CommentSection() {
+  return(
+    <div>
+
+    </div>
+  )
+}
+
+
