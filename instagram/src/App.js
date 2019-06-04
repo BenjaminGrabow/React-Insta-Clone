627d0bcb763442c38fbe93b3443c62e05e9777ee
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-04 14:33:19 +0200

    Added the search functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -25,8 +25,23 @@ class App extends React.Component {
   //   console.log(event)
   // }
 
+  changeSeachResult = (input) => {
+    this.setState({
+      searchInput: input
+    })
+  }
+
+  showSearchResult = (props) => {
+    const copyOfArray = this.state.instaData;
+
+    const searchedItem = copyOfArray.filter(copy => copy.username.toLocaleLowerCase().startsWith(this.state.searchInput.toLocaleLowerCase()));
+
+    this.setState({
+      instaData: searchedItem,
+    })
+  }
+
   makeLike = (props) => {
-    console.log(props);
 
     const copyOfArray = this.state.instaData;
 
@@ -35,24 +50,24 @@ class App extends React.Component {
     const filteredCopyWithTarget = copyOfArray.filter(copy => copy.imageUrl === props.dummyData.imageUrl);
 
     filteredCopyWithTarget.map(value => value.likes += 1);
-    
-    const  result = filteredCopyWithoutTarget.concat(filteredCopyWithTarget);
 
-    this.setState({instaData: result})
+    const result = filteredCopyWithoutTarget.concat(filteredCopyWithTarget);
+
+    this.setState({ instaData: result })
   }
 
   render() {
     return (
       <div className="app">
-        <SearchBar />
+        <SearchBar searchValue={this.state.searchInput} showSearch={this.showSearchResult} changeSearchInput={this.changeSeachResult} />
         {this.state.instaData.map(data =>
           <PostContainer dummyData={data}
             key={uuid()}
             handleLike={this.makeLike}
-            // commentValue={this.state.addFriendInput}
-            // commentChange={event => this.inputComment(event.target.value)}
-            // addAFriend={(event) => this.addFriend(event)}
-             />
+          // commentValue={this.state.addFriendInput}
+          // commentChange={event => this.inputComment(event.target.value)}
+          // addAFriend={(event) => this.addFriend(event)}
+          />
         )}
       </div>
     );
--- instagram/src/Components/SearchBar/SearchBar.js
+++ instagram/src/Components/SearchBar/SearchBar.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from '../../instagram.png';
 
 
-const SearchBar = () => {
+const SearchBar = props => {
         return (
                 <div className="search-bar">
                         <div className="left-header">
@@ -10,7 +10,7 @@ const SearchBar = () => {
                                 <img className="insta-picture" src={logo} alt="insta-logo" />
                         </div>
 
-                        <input placeholder="Search"></input>
+                        <input placeholder="Search" value={props.searchValue} onKeyPress={() => props.showSearch(props)} onChange={event => props.changeSearchInput(event.target.value)}></input>
 
                         <div className="right-header">
                                 <p className="entypo-heart-empty header-icon"></p>
