57efa98ebbf1c8c49b21e3bfa7c4c2fe3d95daa3
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2019-06-06 14:36:08 +0200

    added styled component for the PostContainer

--- instagram/src/Components/PostContainer/PostContainer.js
+++ instagram/src/Components/PostContainer/PostContainer.js
@@ -4,20 +4,35 @@ import pt from 'prop-types';
 import styled from 'styled-components'
 
 const Div = styled.div`
-border: .1rem solid lightgrey;
-  margin: 1rem;
+  border: .1rem solid lightgrey;
+  margin: 0 auto;
+  margin-top: 1rem;
+  width: 50%;
 
-  section
+  section {
+    display: flex;
+  }
+  
+  section img {
+    width: 20%;
+    height: 3rem;
+    border-radius: 50%;
+  }
+
+  span {
+    margin: 1rem;
+  }
 
 
 `;
 
+
 const PostContainer = props => {
   return (
     <Div>
-      <section className="post-top">
-        <img src={props.dummyData.thumbnailUrl} alt="Thumbnail" className="thumbnail" />
-        <p className="username">{props.dummyData.username}</p>
+      <section>
+        <img src={props.dummyData.thumbnailUrl} alt="Thumbnail" />
+        <span>{props.dummyData.username}</span>
       </section>
       <img src={props.dummyData.imageUrl} alt="Post" className="post-img" />
       <div className="icons">
@@ -30,7 +45,7 @@ const PostContainer = props => {
         </div>
       </div>
       <p>{props.dummyData.likes} likes</p>
-      <CommentSection comments={props.dummyData.comments} />  
+      <CommentSection comments={props.dummyData.comments} />
     </Div>
   )
 }
